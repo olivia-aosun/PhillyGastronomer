8ea0086643a5770881ebe86e5c32e02799aa06d3
Yi Wang <wangyi17@seas.upenn.edu>
2018-12-09 14:09:26 -0500

    Jumbotron added

---
+++ Frontend/src/components/ProjectJumbotron/ProjectJumbotron.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Jumbotron, Button } from 'reactstrap';
+
+const ProjectJumbotron = (props) => (
+    <Jumbotron>
+        <h1>Hello, world!</h1>
+        <p>
+            This is a simple hero unit, a simple jumbotron-style component for calling
+            extra attention to featured content or information.
+        </p>
+    </Jumbotron>
+);
+
+export default ProjectJumbotron;
