e83ec6c5f0fb9cd89629397424cc233c6288da5a
JiayWang <jiaywang@seas.upenn.edu>
2018-12-09 19:08:58 -0500

    Update restaurant card

---
+++ Frontend/build/precache-manifest.48d0e5088aa392b368a4e736e5908b9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "47661aa6d0ab2c6e8a5b",
+    "url": "/static/css/main.3096ed14.chunk.css"
+  },
+  {
+    "revision": "47661aa6d0ab2c6e8a5b",
+    "url": "/static/js/main.47661aa6.chunk.js"
+  },
+  {
+    "revision": "619478822b25b2921948",
+    "url": "/static/js/1.61947882.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7d6a07ba6c10be9bb43dc363bd97df28",
+    "url": "/index.html"
+  }
+];
--- Frontend/src/components/RestaurantCard/RestaurantCard.js
+++ Frontend/src/components/RestaurantCard/RestaurantCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classes from './RestaurantCard.css';
 import {
-    Card, CardText, CardBody, Button,
+    Card, CardText, CardBody, Button, ButtonGroup,
     CardTitle, CardSubtitle, Container, Row, Col
 } from 'reactstrap';
 import StarRatings from 'react-star-ratings';
@@ -9,30 +9,47 @@ import StarRatings from 'react-star-ratings';
 const restaurantCard = (props) => {
 
     return (
-        <div>
-            <Card className={classes.card}>
+            <div className={classes.card}>
                 <CardBody>
-                    <CardTitle tag="h3">Restaurant name</CardTitle>
-                    <CardSubtitle tag="h4">Address</CardSubtitle>
-                    <CardText>
+                    <CardTitle tag="h3" className={classes.title}>Restaurant name</CardTitle>
+                    <CardSubtitle tag="h5" className={classes.subtitle}>Address</CardSubtitle>
+                    <div style={{margin: 20}}>
                         <Container>
                             <Row>
-                                <Col>Rating: 
-                                    <StarRatings
-                                        rating={5}
+                                <Col xs={3} md={3}>{'Rating: '} 
+                                    <StarRatings                                       
+                                        rating={5} // change to {props.rating}
                                         starRatedColor="red"
-                                        numberOfStars={5}
-                                        starDimension="15px"
-                                        starSpacing="3px"
+                                        numberOfStars={5} // change to {props.rating}
+                                        starDimension="13px"
+                                        starSpacing="2.5px"
                                     />
                                 </Col>
+                                <Col xs={3} md={3}>Price rage: {props.price_range}</Col>
+                            </Row>
+                            <Row>
+                                <Col xs={3} md={3}>Category: {props.category}</Col>
+                                <Col xs={3} md={3}>Happy hour: {props.happy_hour}</Col>
+                            </Row>
+                            <Row>
+                                <Col xs={3} md={3}>Food quality: {props.food_quality}</Col>
+                                <Col xs={3} md={3}>Service quality: {props.service_quality}</Col>
+                            </Row>
+                            <Row>
+                                <Col xs={3} md={3}>Food quality: {props.food_quality}</Col>
+                                <Col xs={3} md={3}>Service quality: {props.service_quality}</Col>
+                            </Row>
+                            <Row>
+                                <Col xs={2} md={2}>Transit score: {props.transit_score}</Col>
+                                <Col xs={2} md={2}>Walk score: {props.walk_score}</Col>
+                                <Col xs={2} md={2}>Bike score: {props.bike_score}</Col>
                             </Row>
                         </Container>
-                    </CardText>
-                    <Button color="danger">Add to favorite</Button>
+                    </div>
+                    <Button color="danger" className="float-right">Add to favorite</Button>
                 </CardBody>
-            </Card>
-        </div>
+                
+            </div>
     );
 };
 
