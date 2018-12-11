c2306cbb29a0788ce01e00485b989c97f86859ed
Yi Wang <wangyi17@seas.upenn.edu>
2018-12-11 14:44:06 -0500

    Favs and recs added

--- Frontend/src/components/NavBar/NavBar.js
+++ Frontend/src/components/NavBar/NavBar.js
@@ -4,10 +4,13 @@ import Home from '../../containers/Home/Home';
 import ContactPage from '../ContactPage/ContactPage';
 import { Route} from 'react-router-dom';
 import LoginScreen from '../../containers/LoginScreen/LoginScreen';
+import Favorites from '../../containers/Favorites/Favorites';
+import Recommendations from '../../containers/Recommendations/Recommendations';
 let NavItem = require("react-bootstrap/lib/NavItem");
 let Nav = require("react-bootstrap/lib/Nav");
 let MenuItem = require("react-bootstrap/lib/MenuItem");
 let NavDropdown = require("react-bootstrap/lib/NavDropdown");
+
 
 
 class NavBar extends Component {
@@ -34,8 +37,8 @@ class NavBar extends Component {
                     <Nav pullRight>
                         {/* TODO: only show when the user has signed in  */}
                         <NavDropdown eventKey={3} title="My Account" id="basic-nav-dropdown">
-                            <MenuItem eventKey={3.1}>Favorite</MenuItem>
-                            <MenuItem eventKey={3.2}>Recommendation</MenuItem>
+                            <MenuItem eventKey={3.1} href="/favorites">Favorites</MenuItem>
+                            <MenuItem eventKey={3.2} href="/recommendations">Recommendation</MenuItem>
                         </NavDropdown>
                         <NavItem eventKey={2} href="/login">
                             Login
@@ -47,6 +50,9 @@ class NavBar extends Component {
             <Route exact path="/" component={Home}></Route>
             <Route path="/contact" component={ContactPage}></Route>
             <Route path="/login" component={LoginScreen}></Route>
+            <Route path="/favorites" component={Favorites}></Route>
+            <Route path="/recommendations" component={Recommendations}></Route>
+
 
             </div>
         );
